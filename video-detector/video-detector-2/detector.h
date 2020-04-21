70771a51b833b348e0e480ad10cf09f306a142a0
Olga Emelianova <volha.emelianova@gmail.com>
2020-04-21 21:54:12 +0300

    Add new UI

---
+++ video-detector/video-detector-2/detector.h
@@ -0,0 +1,24 @@
+#ifndef DETECTOR_H
+#define DETECTOR_H
+
+#undef slots
+#include "torch/script.h"
+#define slots Q_SLOTS
+#include <opencv2/opencv.hpp>
+#include <string>
+
+class Detector
+{
+public:
+    Detector(std::string modulePath, std::string classNamesPath);
+    ~Detector();
+    cv::Mat detect(cv::Mat originalImage);
+
+private:
+    torch::jit::script::Module m_module;
+    std::vector< std::string > m_classNames;
+
+    torch::Tensor extractResults(torch::Tensor rawPredictions, float confidenceThreshold, float nmsThreshold);
+};
+
+#endif // DETECTOR_H
